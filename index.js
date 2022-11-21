a41ca15c6524ad10d79e1284a39028118905472c
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-11-21 21:44:24 +0800

    mongodb connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,18 +11,25 @@ app.use(express.json());
 
 //mongodb setup
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.bvgttje.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bvgttje.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
+
+    app.get("/", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 
 app.get("/", async (req, res) => {
   res.send("doctors portal server is running");
